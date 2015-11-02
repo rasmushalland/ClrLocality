ecb40c80ab107c85d876520f16986eab1910f6a5
rasmushalland <rasmus@macom.dk>
2015-11-02 23:37:10 +0100

    ComparerBuilder

---
+++ ClrMachineCode.Test/ComparerBuilderTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ClrMachineCode.Test
+{
+	[TestFixture]
+	public class ComparerBuilderTest
+	{
+		[Test]
+		public void Test()
+		{
+			IComparer<MyClass> comparer = ComparerBuilder.Begin<MyClass>().
+				OrderBy(r => r.Name, StringComparer.OrdinalIgnoreCase).
+				ThenBy(r => r.Id).
+				Finish();
+			Assert.That(
+				new[] { new MyClass(1, "henrik"), new MyClass(1, "henrik2") }.OrderBy(r => r, comparer).Select(r => r.Name).ToArray(),
+				Is.EquivalentTo(new[] { "henrik", "henrik2" }));
+			Assert.That(
+				new[] { new MyClass(1, "henrik2"), new MyClass(1, "henrik") }.OrderBy(r => r, comparer).Select(r => r.Name).ToArray(),
+				Is.EquivalentTo(new[] { "henrik", "henrik2" }));
+			Assert.That(
+				new[] { new MyClass(1, "henrik"), new MyClass(1, "HENRIK") }.OrderBy(r => r, comparer).Select(r => r.Name).ToArray(),
+				Is.EquivalentTo(new[] { "henrik", "HENRIK" }));
+			Assert.That(
+				new[] { new MyClass(1, "HENRIK"), new MyClass(1, "henrik") }.OrderBy(r => r, comparer).Select(r => r.Name).ToArray(),
+				Is.EquivalentTo(new[] { "HENRIK", "henrik" }));
+			Assert.That(
+				new[] { new MyClass(2, "HENRIK"), new MyClass(1, "henrik") }.OrderBy(r => r, comparer).Select(r => r.Name).ToArray(),
+				Is.EquivalentTo(new[] { "henrik", "HENRIK" }));
+		}
+
+		private class MyClass
+		{
+			public int Id { get; }
+			public string Name { get; }
+
+			public MyClass(int id, string name)
+			{
+				Id = id;
+				Name = name;
+			}
+		}
+	}
+}
---
+++ ClrMachineCode/ComparerBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClrMachineCode
+{
+	/// <summary>
+	/// The ComparerBuilder classes are handy for constructing implementations of <see cref="IComparer{T}"/>
+	/// with a lambda syntax and convenience close to that of "normal" sorting with .OrderBy().
+	/// </summary>
+	static class ComparerBuilder
+	{
+		public static ComparerBuilderStart<TItem> Begin<TItem>()
+		{
+			return new ComparerBuilderStart<TItem>();
+		}
+	}
+
+	sealed class ComparerBuilderStart<TItem>
+	{
+		public ComparerBuilder<TItem> OrderBy<TKey>(Func<TItem, TKey> keySelector, IComparer<TKey> comparer = null)
+		{
+			return new ComparerBuilder<TItem, TKey>(null, keySelector, comparer, false);
+		}
+		public ComparerBuilder<TItem> OrderByDescending<TKey>(Func<TItem, TKey> keySelector, IComparer<TKey> comparer = null)
+		{
+			return new ComparerBuilder<TItem, TKey>(null, keySelector, comparer, true);
+		}
+	}
+
+	abstract class ComparerBuilder<TItem>
+	{
+		protected ComparerBuilder<TItem> _previous;
+
+		public ComparerBuilder<TItem> ThenBy<TKey>(Func<TItem, TKey> keySelector, IComparer<TKey> comparer = null)
+		{
+			return new ComparerBuilder<TItem, TKey>(this, keySelector, comparer, false);
+		}
+		public ComparerBuilder<TItem> ThenByDescending<TKey>(Func<TItem, TKey> keySelector, IComparer<TKey> comparer = null)
+		{
+			return new ComparerBuilder<TItem, TKey>(this, keySelector, comparer, true);
+		}
+
+		protected abstract Comparison<TItem> CreateComparison();
+		public IComparer<TItem> Finish()
+		{
+			var list = new List<Comparison<TItem>>();
+			var cur = this;
+			while (cur != null)
+			{
+				list.Add(cur.CreateComparison());
+				cur = cur._previous;
+			}
+			list.Reverse();
+
+			return Comparer<TItem>.Create((x, y) => {
+				foreach (var comparison in list)
+				{
+					var res = comparison(x, y);
+					if (res != 0)
+						return res;
+				}
+				return 0;
+			});
+		}
+	}
+
+	sealed class ComparerBuilder<TItem, TKey> : ComparerBuilder<TItem>
+	{
+		private readonly Func<TItem, TKey> _keySelector;
+		private readonly IComparer<TKey> _comparer;
+		private readonly bool _descending;
+		private readonly int _multiplier;
+
+		public ComparerBuilder(ComparerBuilder<TItem> previous, Func<TItem, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+		{
+			_previous = previous;
+			_keySelector = keySelector;
+			_comparer = comparer ?? Comparer<TKey>.Default;
+			_multiplier = descending ? -1 : 1;
+		}
+
+		protected override Comparison<TItem> CreateComparison()
+		{
+			return (x, y) => _comparer.Compare(_keySelector(x), _keySelector(y)) * _multiplier;
+		}
+	}
+}
